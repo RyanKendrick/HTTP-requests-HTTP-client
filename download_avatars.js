8605f10beca028019a43cb38f396b06abcb1eb69
Ryan Kendrick <rpkendrick@gmail.com>
2018-10-08 22:05:53 +0000

    started getReoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,17 @@
+// require `request` and the Node `fs` (filesystem) module
 var request = require('request');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
+
 
