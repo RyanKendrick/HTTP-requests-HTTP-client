dfd6afeee92123750396fb63db2f71b41d08fffe
Ryan Kendrick <rpkendrick@gmail.com>
2018-10-08 22:48:10 +0000

    completed step 6 add gitignore file and commit all changes

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,23 @@
 // require `request` and the Node `fs` (filesystem) module
 var request = require('request');
 var fs = require('fs');
+var token = require('./secrets');
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+    var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + token.GITHUB_TOKEN;
+    }
+  };
+  request(url, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
